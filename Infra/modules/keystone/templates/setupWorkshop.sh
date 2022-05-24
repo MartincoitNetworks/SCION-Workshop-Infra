7fe0941001a721eed23ef96eca1240b04e8ed30d
John Studarus <john@jhlconsulting.com>
2022-05-24 03:23:20 +0000

    OVH key

---
+++ Infra/modules/keystone/templates/setupWorkshop.sh
@@ -0,0 +1,26 @@
+# setup for workshop - addusers, etc...
+
+# add the workshop user
+adduser --disabled-password --gecos "" workshop
+
+# give the workshop user su permissions
+cat > /etc/sudoers.d/91-workshop << EOF
+# place in /etc/sudoers.d/91-workshop
+# chmod 440
+# User rules for workshop
+workshop ALL=(ALL) NOPASSWD:ALL
+EOF
+
+
+
+# workshop public key
+sudo -u workshop mkdir ~workshop/.ssh
+chown workshop ~workshop/.ssh
+chmod 700 ~workshop/.ssh
+sudo -u workshop cat >> ~workshop/.ssh/authorized_keys << EOF
+ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBD2tGw5pMyqma6LZ5SGEptD1v5CivnNY8R/pPJy6m4eHnvL91ihlYYvYxPoydNBbEXu6ABE3xEgBOJlEO/W5ly8= sedna-2022-05-13
+ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBHB4ygmO1/aCmO0VIOa3E79u/Lv7cmXqkOEaBIue3gJlrb77qGmgvTXwf6rgZOCOwCsjgT4STG2R1HtT7lfffj4= workshop@r2-30-de1
+EOF
+chown workshop ~workshop/.ssh/authorized_keys
+chmod 400 ~workshop/.ssh/authorized_keys
+
